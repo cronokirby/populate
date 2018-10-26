09b4853792dce670538f8bdad0ea85eb963ad616
Lúcás Meier <cronokirby@gmail.com>
2018-10-26 14:09:11 +0200

    Add basic type scaffolding
    Add basic definitions for the types we'll be working with

--- src/Populate.hs
+++ src/Populate.hs
@@ -1,6 +1,28 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 module Populate
     ( run
     ) where
+
+import Data.String (IsString)
+import qualified Data.Text as T
+
+
+-- | A URL (wrapper over Text)
+newtype URL = URL T.Text deriving (IsString)
+
+{- | Represents the information related to the source of a Song
+
+Doesn't represent a song directly, but rather a source that may generate multiple
+songs. One example usage would be to represent an album uploaded to a remote source,
+and this would contain information about the author of the album, as well as timestamps
+to split the album into multiple songs.
+-}
+data Source = Source
+    { sourceName :: T.Text
+    , sourceAuthor :: T.Text
+    , sourceURL :: URL
+    }
+
 
 run :: IO ()
 run = putStrLn "someFunc"
