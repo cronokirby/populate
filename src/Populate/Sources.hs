b5db1ea0842b5aba0c98573e19a79ee23d94434b
Lúcás Meier <cronokirby@gmail.com>
2018-10-30 13:02:28 +0100

    Allows overwriting of existing directories

--- src/Populate/Sources.hs
+++ src/Populate/Sources.hs
@@ -21,7 +21,7 @@ import qualified Data.Text as T
 import qualified Data.Text.IO as T
 import qualified Data.Vector as V
 import Sound.TagLib
-import System.Directory (createDirectory)
+import System.Directory (createDirectoryIfMissing)
 import System.IO (FilePath)
 import System.Process (readProcess)
 import Text.Parsec.Error (ParseError)
@@ -218,7 +218,7 @@ splitTimestamps name source timestamps
   | null timestamps =
     addMetadata (name ++ ".m4a") (sourceName source) (sourceArtist source) Nothing
   | otherwise       = do
-    createDirectory name
+    createDirectoryIfMissing True name
     -- The last timestamp is a dummy to let the last segment
     -- be chopped until the end.
     forM_ (zip timestamps (tail timestamps ++ [TimeStamp "" ""])) $ 
